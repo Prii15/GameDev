20f16caeeafc72299d2f5966e5a3590292b6505a
Priscila Vazquez <priscilacv1402@gmail.com>
2025-02-28 14:48:38 -0300

    pong

---
+++ Pong/Assets/SideWalls.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour {
+
+    // Verifica colisões da bola nas paredes
+    void OnTriggerEnter2D (Collider2D hitInfo) {
+        if (hitInfo.tag == "Ball")
+        {
+            string wallName = transform.name;
+            GameManager.Score(wallName);
+            hitInfo.gameObject.SendMessage("RestartGame", null, SendMessageOptions.RequireReceiver);
+        }
+    }
+}
+
